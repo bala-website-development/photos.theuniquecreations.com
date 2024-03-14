23cafb5233d2a0e5e172e29dc4fca53b946eead3
bala <vbalakumar.cse@gmail.com>
2024-03-14 11:26:00 -0500

    changes

--- src/components/qute/index.jsx
+++ src/components/qute/index.jsx
@@ -2,9 +2,15 @@ import PropTypes from "prop-types";
 
 const Qute = ({ data }) => {
   return (
-    <div className="slider-content-area font-dancing" data-aos="fade-up">
-      <h2 dangerouslySetInnerHTML={{ __html: data.qute }}></h2>
-    </div>
+    <>
+      <div className="slider-content-area font-dancing" data-aos="fade-up">
+        <h2 dangerouslySetInnerHTML={{ __html: data.qute }}></h2>
+      </div>
+      <div className="slider-content-area1" data-aos="fade-up">
+        <p className="slider-content-area1" dangerouslySetInnerHTML={{ __html: data.aboutus }}></p>
+      </div>
+      <br />
+    </>
   );
 };
 
--- src/containers/global/page-title/index.jsx
+++ src/containers/global/page-title/index.jsx
@@ -6,7 +6,7 @@ const PageTitleContainer = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-12" data-aos="fade-up">
-            <PageTitle classOption="page-title-content content-style2 text-center" subTitle="About" title="Photography & Creativity" />
+            <PageTitle classOption="page-title-content content-style2 text-center" subTitle="About" title="Photography & Creativity | Digital Arts" />
           </div>
         </div>
       </div>
