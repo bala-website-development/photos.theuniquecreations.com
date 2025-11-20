c3aae400d710d30367f3e074fec5cbd405742bbc
bala <vbalakumar.cse@gmail.com>
2025-11-20 16:43:34 -0600

    added admin link

--- src/layouts/footer/index.jsx
+++ src/layouts/footer/index.jsx
@@ -28,6 +28,9 @@ const Footer = () => {
                       <li>
                         <Link to={process.env.PUBLIC_URL + "/video-gallery"}>Video</Link>
                       </li>
+                      <li>
+                        <Link to={process.env.PUBLIC_URL + "/admin"}>Admin</Link>
+                      </li>
                     </ul>
                   </nav>
                 </div>
